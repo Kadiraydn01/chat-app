baf7620a3b3c098554c8fc4bd69ba2cb9978c889
Kadiraydn01 <kadiraydn01@gmail.com>
2024-07-10 03:17:27 +0300

    backend and frontend can communicate during the signup process

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
-//import Login from "./pages/Login/Login";
-
+import { Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import { Toaster } from "react-hot-toast";
+import { Routes } from "react-router-dom";
 import Homepage from "./pages/Home/Homepage";
 
-//import SignUp from "./pages/SingUp/SignUp";
+import SignUp from "./pages/SingUp/SignUp";
 
 function App() {
   return (
     <>
       <div className="p-4 justify-center items-center flex h-screen">
-        <Homepage />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+        <Toaster />
       </div>
     </>
   );
--- frontend/src/pages/SingUp/SignUp.jsx
+++ frontend/src/pages/SingUp/SignUp.jsx
@@ -1,14 +1,35 @@
+import { Link } from "react-router-dom";
 import GenderCheckbox from "./GenderCheckbox";
+import { useState } from "react";
+import useSignUp from "../../hooks/useSignUp";
 
 const SignUp = () => {
+  const { signup, loading } = useSignUp();
+  const [inputs, setInputs] = useState({
+    fullName: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    gender: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await signup(inputs);
+    console.log(inputs);
+  };
+  const handleCheckbox = (gender) => {
+    setInputs({ ...inputs, gender });
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-w-96 min-h-96 mx-auto">
       <div className=" w-full  p-8 bg-indigo-200 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-50 border border-gray-100">
         <h1 className="text-3xl font-semibold text-center text-gray-600">
-          Sing Up
+          Sign Up
           <span className="text-red-500"> Trixy</span>
         </h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className="label p-2">
               <span className="text-base label-text">Full Name</span>
@@ -17,6 +38,10 @@ const SignUp = () => {
               type="text"
               placeholder="John Doe"
               className="w-full input input-bordered  h-10"
+              value={inputs.fullName}
+              onChange={(e) =>
+                setInputs({ ...inputs, fullName: e.target.value })
+              }
             />
           </div>
 
@@ -28,6 +53,10 @@ const SignUp = () => {
               type="text"
               placeholder="johndoe"
               className="w-full input input-bordered h-10"
+              value={inputs.username}
+              onChange={(e) =>
+                setInputs({ ...inputs, username: e.target.value })
+              }
             />
           </div>
 
@@ -39,6 +68,10 @@ const SignUp = () => {
               type="password"
               placeholder="Enter Password"
               className="w-full input input-bordered h-10"
+              value={inputs.password}
+              onChange={(e) =>
+                setInputs({ ...inputs, password: e.target.value })
+              }
             />
           </div>
 
@@ -50,17 +83,24 @@ const SignUp = () => {
               type="password"
               placeholder="Confirm Password"
               className="w-full input input-bordered h-10"
+              value={inputs.confirmPassword}
+              onChange={(e) =>
+                setInputs({ ...inputs, confirmPassword: e.target.value })
+              }
             />
           </div>
 
-          <GenderCheckbox />
+          <GenderCheckbox
+            onCheckboxChange={handleCheckbox}
+            selectedGender={inputs.gender}
+          />
 
-          <a
+          <Link
             className="text-sm text-center ml-20 hover:underline hover:text-blue-600 mt-2 inline-block"
-            href="#"
+            to="/login"
           >
             Already have an account?
-          </a>
+          </Link>
 
           <div>
             <button className="btn btn-block btn-sm mt-2 border border-slate-700">
